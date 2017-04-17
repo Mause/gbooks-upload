fd4de4adac7800976b27edaf781a69442ba67a54
Dominic May <jack.thatch@gmail.com>
2017-04-17 14:22:44 +0800

    Add standalone uploader

---
+++ upload.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import time
+import types
+import logging
+from pprint import pprint
+from os.path import splitext, basename
+from mimetypes import guess_type, add_type
+
+import httplib2
+from oauth2client.file import Storage
+from oauth2client.tools import run_flow, argparser
+from oauth2client.client import flow_from_clientsecrets
+from apiclient.discovery import build_from_document, build
+
+logging.basicConfig(level=logging.DEBUG)
+add_type('application/epub+zip', '.epub')
+
+
+def get_http():
+    args = argparser.parse_args(["--noauth_local_webserver"])
+    flow = flow_from_clientsecrets(
+        'client_secrets.json',
+        scope=[
+            'https://www.googleapis.com/auth/drive.file',
+            'https://www.googleapis.com/auth/books'
+        ]
+    )
+    storage = Storage('credentials.json')
+    credentials = storage.get()
+    if credentials is None:
+        credentials = run_flow(flags=args, flow=flow, storage=storage)
+    http = credentials.authorize(httplib2.Http())
+    if credentials.access_token_expired:
+        credentials.refresh(http)
+    return http
+
+
+def upload(drive, books, filename):
+    name = splitext(basename(filename))[0]
+    mime_type = guess_type(filename)[0]
+
+    response = drive.files().create(
+        media_body=filename,
+        media_mime_type=mime_type
+    ).execute()
+
+    return books.cloudloading().addBook(
+        # A drive document id. The upload_client_token must not be set.
+        drive_document_id=response['id'],
+
+        # The document MIME type.
+        # It can be set only if the drive_document_id is set.
+        mime_type=mime_type,
+
+        # The document name.
+        # It can be set only if the drive_document_id is set.
+        name=name
+    ).execute()
+
+
+def get_volume(books, name=None, volumeId=None):
+    return next(
+        volume
+        for volume in books.volumes().useruploaded().list().execute()['items']
+        if (
+            volume['volumeInfo']['title'] == name or
+            volume['id'] == volumeId
+        )
+    )
+
+
+def main(argv=sys.argv[1:]):
+    http = get_http()
+    build = lambda filename: build_from_document(open(filename).read(), http=http)
+    books = build('books_v1.json')
+    drive = build('drive_v3.json')
+
+    response = upload(drive, books, argv[0])
+    get = lambda: get_volume(books, volumeId=response['volumeId'])
+
+    wait = 1
+
+    while True:
+        state = get()['userInfo']['userUploadedVolumeInfo']['processingState']
+        print(state)
+        if state.startswith('COMPLETED_'):
+            break
+        time.sleep(wait)
+        wait *= 1.5
+
+
+if __name__ == '__main__':
+    main()
+
