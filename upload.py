bd4ec6d5f8e02504dc0072bcf40342023b6c973e
Elliana May <me@mause.me>
2023-09-20 12:34:31 +0800

    format

--- upload.py
+++ upload.py
@@ -18,28 +18,28 @@ from oauth2client.client import flow_from_clientsecrets
 from googleapiclient.discovery import build_from_document, build, Resource
 
 logging.basicConfig(level=logging.DEBUG)
-add_type('application/epub+zip', '.epub')
+add_type("application/epub+zip", ".epub")
 
-for service in ['books_v1', 'drive_v3']:
-    filename = service + '.json'
+for service in ["books_v1", "drive_v3"]:
+    filename = service + ".json"
     if not exists(filename):
-        url = 'https://www.googleapis.com/discovery/v1/apis/{}/{}/rest'.format(
-            *service.split('_')
+        url = "https://www.googleapis.com/discovery/v1/apis/{}/{}/rest".format(
+            *service.split("_")
         )
-        with open(filename, 'wb') as fh:
+        with open(filename, "wb") as fh:
             fh.write(requests.get(url).content)
 
 
 def get_http():
     args = argparser.parse_args(["--noauth_local_webserver"])
     flow = flow_from_clientsecrets(
-        'client_secrets.json',
+        "client_secrets.json",
         scope=[
-            'https://www.googleapis.com/auth/drive.file',
-            'https://www.googleapis.com/auth/books',
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/books",
         ],
     )
-    storage = Storage('credentials.json')
+    storage = Storage("credentials.json")
     credentials = storage.get()
     if credentials is None:
         credentials = run_flow(flags=args, flow=flow, storage=storage)
@@ -61,7 +61,7 @@ def upload(drive, books, filename):
         books.cloudloading()
         .addBook(
             # A drive document id. The upload_client_token must not be set.
-            drive_document_id=response['id'],
+            drive_document_id=response["id"],
             # The document MIME type.
             # It can be set only if the drive_document_id is set.
             mime_type=mime_type,
@@ -76,8 +76,8 @@ def upload(drive, books, filename):
 def get_volume(books: Resource, name: str = None, volumeId: str = None):
     return next(
         volume
-        for volume in books.volumes().useruploaded().list().execute()['items']
-        if (volume['volumeInfo']['title'] == name or volume['id'] == volumeId)
+        for volume in books.volumes().useruploaded().list().execute()["items"]
+        if (volume["volumeInfo"]["title"] == name or volume["id"] == volumeId)
     )
 
 
@@ -88,28 +88,28 @@ def main():
 
     http = get_http()
     build = lambda filename: build_from_document(open(filename).read(), http=http)
-    books = build('books_v1.json')
-    drive = build('drive_v3.json')
+    books = build("books_v1.json")
+    drive = build("drive_v3.json")
 
     for filename in args.files:
         response = upload(drive, books, filename)
 
-        monitor(books, response['volumeId'])
+        monitor(books, response["volumeId"])
 
 
 def monitor(books: Resource, volume_id: str) -> None:
     wait = 1
 
     while True:
-        state = get_volume(books, volumeId=volume_id)['userInfo'][
-            'userUploadedVolumeInfo'
-        ]['processingState']
+        state = get_volume(books, volumeId=volume_id)["userInfo"][
+            "userUploadedVolumeInfo"
+        ]["processingState"]
         print(state)
-        if state.startswith('COMPLETED_'):
+        if state.startswith("COMPLETED_"):
             break
         time.sleep(wait)
         wait *= 1.5
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
