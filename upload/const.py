a0d23accbdb4947e3abf2f7ca0af1396bdb7c324
Elliana May <me@mause.me>
2025-01-15 14:51:48 +0000

    correct package name

--- upload/const.py
+++ upload/const.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from appdirs import user_cache_dir
+from platformdirs import user_cache_dir
 
 PATH = Path(user_cache_dir("gbooks-upload", "Elliana May"))
 COOKIE_TXT = PATH / "cookie.txt"
