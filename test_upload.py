f7da1983b18d7946ca73658846e343ae2ff6026a
Elliana May <me@mause.me>
2025-01-20 13:41:30 +0800

    add test for non existent file

--- test_upload.py
+++ test_upload.py
@@ -12,3 +12,10 @@ def test_hello_world(snapshot):
     result = runner.invoke(upload, ["--help"])
     assert result.exit_code == 0
     assert result.output == snapshot
+
+
+def test_non_existing_file(snapshot):
+    runner = CliRunner(echo_stdin=True)
+    result = runner.invoke(upload, ["upload", "dummy.epub"])
+    assert result.exit_code == 2
+    assert result.output == snapshot
