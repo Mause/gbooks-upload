ad44360c7c789fba5a864a8995fc53c3f9c3a623
Elliana May <me@mause.me>
2025-02-02 16:38:53 +0800

    chore: move google_internal_apis tests

--- test_upload.py
+++ test_upload.py
@@ -1,13 +1,8 @@
-import json
 import os
 
 from click.testing import CliRunner
-from google.protobuf.json_format import MessageToDict
 
 from gbooks_upload import main as upload
-from gbooks_upload.endpoints import parse
-from google_internal_apis.json_format import dump
-from input_pb2 import LibraryDocumentResponse, TagsResponse
 
 os.environ["TERM"] = "dumb"
 
@@ -17,32 +12,3 @@ def test_hello_world(snapshot):
     result = runner.invoke(upload, ["--help"])
     assert result.exit_code == 0
     assert result.output == snapshot
-
-
-def test_protoc(snapshot):
-    original = [
-        [
-            ["tag_name_1", "tag_id_1", "1"],
-            ["tag_name_2", "tag_id_2", "2"],
-        ],
-        [
-            ["book_id_1", "tag_id_1", "1"],
-            ["book_id_2", "tag_id_2", "2"],
-        ],
-    ]
-    out = parse(
-        original,
-        TagsResponse(),
-    )
-    assert str(out).strip() == snapshot
-    assert MessageToDict(out) == snapshot
-
-    assert dump(out) == original
-
-
-def test_parse_complex(snapshot):
-    with open("complex.json") as f:
-        payload = json.load(f)
-    out = parse(payload, LibraryDocumentResponse())
-    assert str(out).strip() == snapshot
-    assert MessageToDict(out) == snapshot
