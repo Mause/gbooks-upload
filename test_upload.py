048267aa20399d718009e9709dd8f089d0b9d828
Elliana May <me@mause.me>
2025-02-06 04:06:53 +0000

    test: assert on group help

--- test_upload.py
+++ test_upload.py
@@ -1,14 +1,16 @@
 import os
 
 from click.testing import CliRunner
+from pytest import mark
 
 from gbooks_upload import main as upload
 
 os.environ["TERM"] = "dumb"
 
 
-def test_hello_world(snapshot):
+@mark.parametrize("subcommand", (None, "books", "shelves"))
+def test_hello_world(snapshot, subcommand):
     runner = CliRunner()
-    result = runner.invoke(upload, ["--help"])
+    result = runner.invoke(upload, ([subcommand] if subcommand else []) + ["--help"])
     assert result.exit_code == 0
     assert result.output == snapshot
