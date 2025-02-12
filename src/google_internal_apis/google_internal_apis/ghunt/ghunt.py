35ef2a80b644ca9baec40222427281bb00b59118
Elliana May <me@mause.me>
2025-02-12 15:44:46 +0800

    fmt

--- src/google_internal_apis/google_internal_apis/ghunt/ghunt.py
+++ src/google_internal_apis/google_internal_apis/ghunt/ghunt.py
@@ -3,9 +3,11 @@ import sys
 
 def main():
     version = sys.version_info
-    if (version < (3, 10)):
-        print('[-] GHunt only works with Python 3.10+.')
-        print(f'Your current Python version : {version.major}.{version.minor}.{version.micro}')
+    if version < (3, 10):
+        print("[-] GHunt only works with Python 3.10+.")
+        print(
+            f"Your current Python version : {version.major}.{version.minor}.{version.micro}"
+        )
         sys.exit(1)
 
     from ghunt.cli import parse_and_run
--- src/google_internal_apis/google_internal_apis/ghunt/modules/geolocate.py
+++ src/google_internal_apis/google_internal_apis/ghunt/modules/geolocate.py
@@ -11,12 +11,14 @@ from pathlib import Path
 import json
 
 
-async def main(as_client: httpx.AsyncClient, bssid: str, input_file: Path, json_file: Path=None):
+async def main(
+    as_client: httpx.AsyncClient, bssid: str, input_file: Path, json_file: Path = None
+):
     # Verifying args
     body = None
     if input_file:
         if not input_file.exists():
-            exit(f"[-] The input file \"{input_file}\" doesn't exist.")
+            exit(f'[-] The input file "{input_file}" doesn\'t exist.')
         with open(input_file, "r", encoding="utf-8") as f:
             try:
                 body = json.load(f)
@@ -25,7 +27,7 @@ async def main(as_client: httpx.AsyncClient, bssid: str, input_file: Path, json_
 
     if not as_client:
         as_client = get_httpx_client()
- 
+
     ghunt_creds = await auth.load_and_auth(as_client)
 
     geo_api = GeolocationHttp(ghunt_creds)
@@ -34,8 +36,10 @@ async def main(as_client: httpx.AsyncClient, bssid: str, input_file: Path, json_
         exit("[-] The location wasn't found.")
 
     geolocator = Nominatim(user_agent="nominatim")
-    location = geolocator.reverse(f"{resp.location.latitude}, {resp.location.longitude}", timeout=10)
-    raw_address = location.raw['address']
+    location = geolocator.reverse(
+        f"{resp.location.latitude}, {resp.location.longitude}", timeout=10
+    )
+    raw_address = location.raw["address"]
     address = location.address
 
     gb.rc.print("📍 Location found !\n", style="plum2")
@@ -43,18 +47,28 @@ async def main(as_client: httpx.AsyncClient, bssid: str, input_file: Path, json_
     gb.rc.print(f"Latitude : {resp.location.latitude}", style="bold")
     gb.rc.print(f"Longitude : {resp.location.longitude}\n", style="bold")
     gb.rc.print(f"🏠 Estimated address : {address}\n")
-    gb.rc.print(f"🗺️ [italic][link=https://www.google.com/maps/search/?q={resp.location.latitude},{resp.location.longitude}]Open in Google Maps[/link][/italic]\n", style=f"cornflower_blue")
+    gb.rc.print(
+        f"🗺️ [italic][link=https://www.google.com/maps/search/?q={resp.location.latitude},{resp.location.longitude}]Open in Google Maps[/link][/italic]\n",
+        style=f"cornflower_blue",
+    )
 
     if json_file:
-        from ghunt.objects.encoders import GHuntEncoder;
+        from ghunt.objects.encoders import GHuntEncoder
+
         with open(json_file, "w", encoding="utf-8") as f:
-            f.write(json.dumps({
-                "accuracy": resp.accuracy,
-                "latitude": resp.location.latitude,
-                "longitude": resp.location.longitude,
-                "address": raw_address,
-                "pretty_address": address
-            }, cls=GHuntEncoder, indent=4))
+            f.write(
+                json.dumps(
+                    {
+                        "accuracy": resp.accuracy,
+                        "latitude": resp.location.latitude,
+                        "longitude": resp.location.longitude,
+                        "address": raw_address,
+                        "pretty_address": address,
+                    },
+                    cls=GHuntEncoder,
+                    indent=4,
+                )
+            )
         gb.rc.print(f"[+] JSON output wrote to {json_file} !", style="italic")
 
     await as_client.aclose()
