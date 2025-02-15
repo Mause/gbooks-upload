81fd1886eb2ed103f23bbcf0988246475c0599b0
Elliana May <me@mause.me>
2025-02-15 14:21:35 +0800

    restore parse_oauth_flow_response

--- src/google_internal_apis/google_internal_apis/ghunt/helpers/utils.py
+++ src/google_internal_apis/google_internal_apis/ghunt/helpers/utils.py
@@ -1,10 +1,12 @@
-# from PIL import Image
 import hashlib
+import logging
 from copy import deepcopy
 from time import time
 from typing import Dict
 
 import httpx
+
+logger = logging.getLogger(__name__)
 
 
 def gen_sapisidhash(
@@ -25,5 +27,14 @@ def is_headers_syntax_good(headers: Dict[str, str]) -> bool:
     try:
         httpx.Headers(headers)
         return True
-    except:
+    except Exception:
+        logger.error("Invalid headers syntax")
         return False
+
+
+def parse_oauth_flow_response(body: str):
+    """
+    Correctly format the response sent by android.googleapis.com
+    during the Android OAuth2 Login Flow.
+    """
+    return {sp[0]: "=".join(sp[1:]) for x in body.split("\n") if (sp := x.split("="))}
